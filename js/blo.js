d27e7668405d74dfb48d4de65611b3586df18678
Seanna Lea LoBue <qarylla@gmail.com>
2017-08-17 09:14:46 -0400

    Update index.html to include jquery and add initial JS file.

---
+++ js/blo.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+
+$(".info").html("<p>Open Tuesday-Saturday with flexible hours.</p>
+			<p>Call us at 859 523 9797.</p>
+			<p>Located at 410 South Broadway, Lexington, KY 40508.</p>");
+
+});
