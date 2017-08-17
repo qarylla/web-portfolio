81ac6e4557c2d33a74f4894dd9d3e1786aa4d4b4
Seanna Lea LoBue <qarylla@gmail.com>
2017-08-17 10:18:43 -0400

    Update jQuery for initial load; switch CSS from a class to an id.

--- js/blo.js
+++ js/blo.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
 
-$(".info").html("<p>Open Tuesday-Saturday with flexible hours.</p>
-			<p>Call us at 859 523 9797.</p>
-			<p>Located at 410 South Broadway, Lexington, KY 40508.</p>");
-
+	$("#info").html("<p>Open Tuesday-Saturday with flexible hours.</p><p>Call at 859.523.9797.</p><p>Located at 410 South Broadway, Lexington, KY 40508</p>");
+	
+	
 });
